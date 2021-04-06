fe53a30919caa32df1561e34dcf6d41e31b1d4fd
Joe <overtonje@gmail.com>
2021-04-06 17:08:15 -0500

    signing off

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -3,6 +3,7 @@ defmodule GameTest do
 
 alias Hangman.Game
 
+
 test "new_game returns structure" do
     game = Game.new_game()
 
@@ -10,6 +11,8 @@ test "new_game returns structure" do
     assert game.game_state == :initializing
     assert length(game.letters) > 0
     assert length(game.letters) == length(Enum.filter(game.letters, fn (letter) ->  String.match?(letter, ~r/\p{Ll}/) == true end))
+
+
 
 end
 
