ea2802c087470fc117c0769fbd973866ab6b7864
Joe <overtonje@gmail.com>
2021-04-06 11:14:17 -0500

    wrote basic tests for game

--- hangman/test/hangman_test.exs
+++ hangman/test/hangman_test.exs
@@ -2,14 +2,5 @@ defmodule HangmanTest do
   use ExUnit.Case
   doctest Hangman
 
-  test "greets the world" do
-    assert Hangman.hello() == :world
-  end
-
-
-  test "the truth" do
-    assert [1,2,3] == [2,2,3]
-    
-  end
 
 end
