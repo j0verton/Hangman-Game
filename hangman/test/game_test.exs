94ea09c6458ca53a9df9c65dc5aa8207fdcd9b2a
Joe <overtonje@gmail.com>
2021-04-07 12:08:07 -0500

    two working tests

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -1,26 +1,26 @@
 defmodule GameTest do
   use ExUnit.Case
 
-alias Hangman.Game
+  alias Hangman.Game
 
 
-test "new_game returns structure" do
+  test "new_game returns structure" do
+      game = Game.new_game()
+
+      assert game.turns_left == 7
+      assert game.game_state == :initializing
+      assert length(game.letters) > 0
+      assert length(game.letters) == length(Enum.filter(game.letters, fn (letter) ->  String.match?(letter, ~r/\p{Ll}/) == true end))
+
+
+
+  end
+
+  test "state isn't changed for :won game" do
     game = Game.new_game()
+      |>Map.put(:game_state, :won)
+    assert{^game, _} = Game.make_move(game, "x")
+  
 
-    assert game.turns_left == 7
-    assert game.game_state == :initializing
-    assert length(game.letters) > 0
-    assert length(game.letters) == length(Enum.filter(game.letters, fn (letter) ->  String.match?(letter, ~r/\p{Ll}/) == true end))
-
-
-
-end
-
-test "state isn't changed for :won game"
-game = Game.new_game()
-  |> Map.put(:game_state, :won)
-assert {^game, _} = Game.make_move(game, "x")
-
-
-
+  end 
 end
