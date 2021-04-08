ffe7efade2c2ba7a6db5655db9c27987b8fa2a22
Joe <overtonje@gmail.com>
2021-04-08 13:11:12 -0500

    added make move for already guessed and go ahead working on tests

--- hangman/test/game_test.exs
+++ hangman/test/game_test.exs
@@ -3,25 +3,34 @@ defmodule GameTest do
 
   alias Hangman.Game
 
-
   test "new_game returns structure" do
-      game = Game.new_game()
+    game = Game.new_game()
 
-      assert game.turns_left == 7
-      assert game.game_state == :initializing
-      assert length(game.letters) > 0
-      assert length(game.letters) == length(Enum.filter(game.letters, fn (letter) ->  String.match?(letter, ~r/\p{Ll}/) == true end))
+    assert game.turns_left == 7
+    assert game.game_state == :initializing
+    assert length(game.letters) > 0
 
-
-
+    assert length(game.letters) ==
+             length(
+               Enum.filter(game.letters, fn letter ->
+                 String.match?(letter, ~r/\p{Ll}/) == true
+               end)
+             )
   end
 
   test "state isn't changed for :won or :lost game" do
-  for state <- [:won, :lost] do
-      game = Game.new_game()
-        |>Map.put(:game_state, :won)
-      assert{^game, _} = Game.make_move(game, "x")
-    end
-  end 
+    for state <- [:won, :lost] do
+      game =
+        Game.new_game()
+        |> Map.put(:game_state, :won)
 
+      assert {^game, _} = Game.make_move(game, "x")
+    end
+  end
+
+  test "first occurrence of letter is not already used" do
+    game = Game.new_game()
+    {game, _tally} = Game.make_move(game, "x")
+    assert game.game_state != :already_used
+  end
 end
